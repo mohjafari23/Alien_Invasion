e9733259c73cb1c4b4e27647977d4871e08beff7
Mohammad Hossein <136825585+mohjafari23@users.noreply.github.com>
2024-09-15 13:44:04 +0330

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,4 +9,9 @@ class settings:
         self.set_caption = "Alien Invasion"
         # Ship settings
         self.ship_speed = 1.5
-        
+        #Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 5
